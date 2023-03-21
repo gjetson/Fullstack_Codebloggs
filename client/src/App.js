e7e4e9c2811940ce6ff676b2574f3f28beea79dd
Del Barnwell <dbarnwel@google.com>
2023-03-21 15:19:29 -0400

    initial manpage and routing is in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import Sidebar from "./components/Sidebar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { AboutUs, OurAim, OurVision } from "./tmp/pages/AboutUs"
+import { Services, ServicesOne, ServicesTwo, ServicesThree, } from "./tmp/pages/Services"
+import { Events, EventsOne, EventsTwo } from "./tmp/pages/Events"
+import Contact from "./tmp/pages/ContactUs"
+import Support from "./tmp/pages/Support"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/about-us/aim' element={<OurAim />} />
+        <Route path='/about-us/vision' element={<OurVision />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/services/services1' element={<ServicesOne />} />
+        <Route path='/services/services2' element={<ServicesTwo />} />
+        <Route path='/services/services3' element={<ServicesThree />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/events/events1' element={<EventsOne />} />
+        <Route path='/events/events2' element={<EventsTwo />} />
+        <Route path='/support' element={<Support />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
