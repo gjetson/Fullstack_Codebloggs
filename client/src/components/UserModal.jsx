78e40c03b7bf2d1e5ed987672644e7dbe5cd7cdd
x.mat1 <mat12345duq6789@gmail.com>
2023-03-28 14:18:07 -0400

    network user modal format ready waiting for backend

--- client/src/components/UserModal.jsx
+++ client/src/components/UserModal.jsx
@@ -5,29 +5,48 @@ import { useState } from 'react'
 
 
 function UserModal(props) {
-    const [post, setPost] = useState("");
     const [isOpen, setIsOpen] = useState(true);
-  
+
     const handleClose = () => {
-      setIsOpen(false);
-      props.onClose();
+        setIsOpen(false);
+        props.onClose();
     };
-  
+
     if (!isOpen) {
-      return null;
+        return null;
     }
-  
+
+    const { user } = props;
+
     return (
-      <div className="custom-confirm">
-        <h1 className="modal-header">
-          User Card
-          <button className="close-button" onClick={handleClose}>
-            X
-          </button>
-        </h1>
-      </div>
+        <div className="network-modal">
+            <h1 className="network-modal-header">
+                User Card
+                <button className="close-button" onClick={handleClose}>
+                    X
+                </button>
+            </h1>
+            <div className="user-profile">
+                <div className="profile-picture">JS</div>
+                <div className="user-info">
+                    <h2>John Smith</h2>
+                    <p>Tampa</p>
+                </div>
+            </div>
+            <hr className="divider" />
+            <div className="user-status">
+                <h3>Status</h3>
+                <p>Placeholder for user status</p>
+            </div>
+            <hr className="divider" />
+            <div className="latest-user-post">
+                <h3>Latest Post</h3>
+                <p>Placeholder for latest user post</p>
+            </div>
+
+        </div>
     );
-  }
-  
+}
+
 
 export default UserModal
