b3c61c21d682e73df342d7c488861cb22bfd4cac
Del Barnwell <dbarnwel@google.com>
2023-03-30 15:31:03 -0400

    added get comments by post id

--- server/src/routes/comment_routes.js
+++ server/src/routes/comment_routes.js
@@ -3,7 +3,7 @@ const Controller = require('../controllers/comment_controller')
 const registerCommentRoutes = (app) => {
     app.post('/comment', Controller.createComment)
     app.get('/comments', Controller.getComments)
-    app.get('/comment/:id', Controller.getComment)
+    app.get('/comments/:postId', Controller.getCommentsByPostId)
     app.post('/comment/like/:id', Controller.addLike)
     app.post('/comment/update/:id', Controller.updateComment)
     app.delete('/comment/delete/:id', Controller.deleteComment)
