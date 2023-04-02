8afe5e4b0f47949a5fde75136aabd4105db38ef0
Del Barnwell <dbarnwel@google.com>
2023-04-02 17:40:41 -0400

    fixed a bug on the user bloggs page to allow it to display when the user hadn't posted yet

--- client/src/components/UserBloggs.jsx
+++ client/src/components/UserBloggs.jsx
@@ -53,46 +53,46 @@ export default function UserBloggs() {
             }
         })
     }
-
-    return (
-        <div style={{ marginLeft: '250px' }}>
-            {bloggs.map((blogg, index) => {
-                if (index === 0) {
-                    return (
-                        <MDBRow key={index}>
-                            <MDBCol sm='5'>
-                                <MDBCard>
-                                    <MDBCardBody>
-                                        <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
-                                        <MDBCardTitle>{user.first_name} {user.last_name}</MDBCardTitle>
-                                        <MDBCardText>
-                                            {user.status}
-                                        </MDBCardText>
-                                    </MDBCardBody>
-                                </MDBCard>
-                            </MDBCol>
-                            <MDBCol sm='1'></MDBCol>
-                            <MDBCol sm='5'>
-                                <MDBCard>
-                                    <MDBCardBody>
-                                        <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
-                                        <MDBCardTitle>{blogg.content}</MDBCardTitle>
-                                        {/* <MDBCardText>
+    if (bloggs && bloggs.length > 0) {
+        return (
+            <div style={{ marginLeft: '250px' }}>
+                {bloggs.map((blogg, index) => {
+                    if (index === 0) {
+                        return (
+                            <MDBRow key={index}>
+                                <MDBCol sm='5'>
+                                    <MDBCard>
+                                        <MDBCardBody>
+                                            <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
+                                            <MDBCardTitle>{user.first_name} {user.last_name}</MDBCardTitle>
+                                            <MDBCardText>
+                                                {user.status}
+                                            </MDBCardText>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                                <MDBCol sm='1'></MDBCol>
+                                <MDBCol sm='5'>
+                                    <MDBCard>
+                                        <MDBCardBody>
+                                            <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
+                                            <MDBCardTitle>{blogg.content}</MDBCardTitle>
+                                            {/* <MDBCardText>
                                             With supporting text below as a natural lead-in to additional content.
                                         </MDBCardText> */}
-                                        <MDBBtn onClick={() => { handleCardClick(index) }}>Show Blogg</MDBBtn>
-                                    </MDBCardBody>
-                                </MDBCard>
-                            </MDBCol>
-                            <MDBCol sm='1'></MDBCol>
-                        </MDBRow>
-                    )
-                } else {
-                    return (
-                        <MDBRow key={index}>
-                            <MDBCol sm='5'>
-                                <MDBCard>
-                                    {/* <MDBCardBody>
+                                            <MDBBtn onClick={() => { handleCardClick(index) }}>Show Blogg</MDBBtn>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                                <MDBCol sm='1'></MDBCol>
+                            </MDBRow>
+                        )
+                    } else {
+                        return (
+                            <MDBRow key={index}>
+                                <MDBCol sm='5'>
+                                    <MDBCard>
+                                        {/* <MDBCardBody>
                                         <MDBCardImage src={`https://picsum.photos/seed/${getRandomInt(100)}/300/100`} alt='...' position='top' />
                                         <MDBCardTitle>Special title treatment</MDBCardTitle>
                                         <MDBCardText>
@@ -100,26 +100,59 @@ export default function UserBloggs() {
                                         </MDBCardText>
                                         <MDBBtn href='#'>Go somewhere</MDBBtn>
                                     </MDBCardBody> */}
-                                </MDBCard>
-                            </MDBCol>
-                            <MDBCol sm='1'></MDBCol>
-                            <MDBCol sm='5'>
-                                <MDBCard>
-                                    <MDBCardBody>
-                                        <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
-                                        <MDBCardTitle>{blogg.content}</MDBCardTitle>
-                                        {/* <MDBCardText>
+                                    </MDBCard>
+                                </MDBCol>
+                                <MDBCol sm='1'></MDBCol>
+                                <MDBCol sm='5'>
+                                    <MDBCard>
+                                        <MDBCardBody>
+                                            <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
+                                            <MDBCardTitle>{blogg.content}</MDBCardTitle>
+                                            {/* <MDBCardText>
                                             With supporting text below as a natural lead-in to additional content.
                                         </MDBCardText> */}
-                                        <MDBBtn onClick={() => { handleCardClick(index) }}>Show Blogg</MDBBtn>
-                                    </MDBCardBody>
-                                </MDBCard>
-                            </MDBCol>
-                            <MDBCol sm='1'></MDBCol>
-                        </MDBRow>
-                    )
-                }
-            })}
-        </div>
-    )
+                                            <MDBBtn onClick={() => { handleCardClick(index) }}>Show Blogg</MDBBtn>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                                <MDBCol sm='1'></MDBCol>
+                            </MDBRow>
+                        )
+                    }
+                })}
+            </div>
+        )
+    } else {
+        return (
+            <div style={{ marginLeft: '250px' }}>
+                <MDBRow key={0}>
+                    <MDBCol sm='5'>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
+                                <MDBCardTitle>{user.first_name} {user.last_name}</MDBCardTitle>
+                                <MDBCardText>
+                                    {user.status}
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol sm='1'></MDBCol>
+                    <MDBCol sm='5'>
+                        <MDBCard>
+                            {/* <MDBCardBody>
+                            <MDBCardImage src={`https://picsum.photos/seed/${userActions.getRandomInt(100)}/300/100`} alt='...' position='top' />
+                            <MDBCardTitle>{blogg.content}</MDBCardTitle>
+                            <MDBCardText>
+                            With supporting text below as a natural lead-in to additional content.
+                        </MDBCardText>
+                            <MDBBtn onClick={() => { handleCardClick(index) }}>Show Blogg</MDBBtn>
+                        </MDBCardBody> */}
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol sm='1'></MDBCol>
+                </MDBRow>
+            </div>
+        )
+    }
 }
